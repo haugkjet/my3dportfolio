f03ef602ed7f7a414c0329d74460b13b8c80734f
Kjetil <haugkjet@gmail.com>
2023-12-04 14:24:12 +0100

    Added gobot from gdquest with idle animation

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -4,6 +4,7 @@ import { OrbitControls} from '@react-three/drei'
 import { Perf } from 'r3f-perf'
 import { DoubleSide } from "three";
 import Player from './Player'
+import Lights from './Lights'
 
 export default function Experience() {
     return (
@@ -18,6 +19,8 @@ export default function Experience() {
         } } style={{ background: '#8CABFF' }}>
        <Perf position="top-left" />      
        <OrbitControls />
+       <Lights/>
+       
        <gridHelper args={[50, 50, 0x35155D, '#512B81']} />
 
       <mesh position={[0, -0.01, 0]} rotation={[Math.PI / 2, 0, 0]} scale={[50, 50, 1]}>
--- src/Player.jsx
+++ src/Player.jsx
@@ -1,9 +1,18 @@
+import React, {useEffect, useRef} from "react"
+import { useGLTF, useAnimations } from '@react-three/drei'
+import { Model } from "./Gobot"
+
 export default function Player() {
+  
+
     return (
         <>
-      <mesh position={[0, 0.5, 0]} rotation={[0, 0, 0]} scale={[1, 1, 1]}>
+              
+          <Model/>
+                 
+     {/*} <mesh position={[0, 0.5, 0]} rotation={[0, 0, 0]} scale={[1, 1, 1]}>
       <boxGeometry attach="geometry" />
       <meshBasicMaterial attach="material" color="#8CABFF" />
-    </mesh>
+    </mesh>*/}
     </>
 )}
