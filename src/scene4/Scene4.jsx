5aa55261f008df0e16f6307c0a0a78afc1e4f2b6
Kjetil <haugkjet@gmail.com>
2024-02-12 10:11:50 +0100

    Added more charts and panels

--- src/scene4/Scene4.jsx
+++ src/scene4/Scene4.jsx
@@ -57,13 +57,106 @@ export default function Scene3({ textureCube }) {
 
       <S4Lights />
       <Ground />
-      <PanelExtruded position={[-10, 0.21, 5]} scale={[3, 3, 1]} />
-      <PanelExtruded position={[-4, 0.21, 5]} scale={[3, 3, 1]} />
-      <PanelExtruded position={[2, 0.21, 5]} scale={[3, 3, 1]} />
+
+      {/*Behind row panels*/}
+      <PanelExtruded position={[-10, 0.21, 2]} scale={[3, 3, 1]} />
+      <PanelExtruded position={[-4, 0.21, 2]} scale={[3, 3, 1]} />
+      <PanelExtruded position={[2, 0.21, 2]} scale={[3, 3, 1]} />
+
+      {/*Behind row charts*/}
+      <group scale={[0.5, 0.5, 0.5]}>
+        <Barchart
+          posx={21.5}
+          posy={0.9}
+          posz={4}
+          length={3}
+          roty={0}
+          title={""}
+          maxheight={5}
+        />
+      </group>
+      <group scale={[0.3, 0.3, 0.3]}>
+        <Barchart
+          posx={17}
+          posy={1.5}
+          posz={4}
+          length={7}
+          roty={0}
+          title={""}
+          maxheight={10}
+        />
+      </group>
+      <group scale={[0.3, 0.3, 0.3]}>
+        <gridHelper
+          args={[10, 10, "orange", "#ededed"]}
+          position={[6.5, 1.5, 7]}
+          rotation={[0, 0, 0]}
+        />
+      </group>
+
+      <group scale={[0.3, 0.3, 0.3]}>
+        <gridHelper
+          args={[10, 10, "#ededed", "#ededed"]}
+          position={[-13, 1.5, 7]}
+          rotation={[0, 0, 0]}
+        />
+      </group>
+
+      {/*Middle row panels*/}
+      <PanelExtruded position={[-4, 0.21, 6]} scale={[3, 3, 1]} />
+      <PanelExtruded position={[2, 0.21, 6]} scale={[3, 3, 1]} />
       <PanelExtruded position={[-4, 0.21, 9]} scale={[1, 2, 1]} />
 
+      {/*Middle row charts*/}
+      <MyPieChart
+        data={data1}
+        position={[2, 0.5, 6]}
+        radius={1.2}
+        title={"Sales"}
+        depth={0.1}
+      />
+
+      <MyPieChart
+        data={data1}
+        position={[-4, 0.5, 6]}
+        radius={1.2}
+        title={"Sales"}
+        depth={0.1}
+      />
+
+      <group scale={[0.3, 0.3, 0.3]}>
+        <Barchart
+          posx={24}
+          posy={1.5}
+          posz={-37}
+          length={7}
+          roty={Math.PI / 2}
+          title={""}
+          maxheight={10}
+        />
+      </group>
+
+      {/*Front row panels*/}
+      <PanelExtruded position={[-10, 0.21, 6]} scale={[3, 3, 1]} />
       <PanelExtruded position={[2, 0.21, 9]} scale={[2, 1, 1]} />
 
+      {/*Front row charts*/}
+      <MyPieChart
+        data={data1}
+        position={[2.5, 0.5, 9]}
+        radius={0.3}
+        title={"Sales"}
+        depth={0.05}
+      />
+      <MyPieChart
+        data={data1}
+        position={[1.5, 0.5, 9]}
+        radius={0.3}
+        title={"EBITDA"}
+        depth={0.05}
+      />
+
+      {/* Wall behind panels*/}
       <group rotation={[Math.PI / 2, 0, 0]}>
         <PanelExtruded position={[-8, -0.6, -5]} scale={[3, 3, 1]} />
       </group>
@@ -72,29 +165,11 @@ export default function Scene3({ textureCube }) {
         <PanelExtruded position={[2, -0.6, -5]} scale={[3, 3, 1]} />
       </group>
 
-      <group scale={[0.5, 0.5, 0.5]}>
-        <Barchart
-          posx={21.5}
-          posy={0.9}
-          posz={10}
-          length={3}
-          roty={0}
-          title={""}
-          maxheight={5}
-        />
-      </group>
-      <group scale={[0.5, 0.5, 0.5]}>
-        <Barchart
-          posx={10}
-          posy={0.9}
-          posz={10}
-          length={3}
-          roty={0}
-          title={""}
-          maxheight={5}
-        />
+      <group rotation={[Math.PI / 2, 0, 0]}>
+        <PanelExtruded position={[-3, -0.6, -5]} scale={[3, 3, 1]} />
       </group>
 
+      {/* Behind wall*/}
       <RoundedBox
         args={[22.5, 12, 0.25]}
         position={[-2, 6, -0.7]}
@@ -112,6 +187,47 @@ export default function Scene3({ textureCube }) {
           Light test
         </Text>
       </RoundedBox>
+
+      <group scale={[1, 0.4, 0.55]}>
+        <gridHelper
+          args={[20, 10, "orange", "white"]}
+          position={[-2.5, 12, -1]}
+          rotation={[Math.PI / 2, 0, 0]}
+        />
+      </group>
+
+      {/* Behind wall charts*/}
+      <group rotation={[Math.PI / 2, 0, 0]}>
+        <MyPieChart
+          data={data1}
+          position={[2, -0.25, -5.2]}
+          radius={1.2}
+          title={"Sales"}
+          depth={0.1}
+        />
+      </group>
+      <group rotation={[Math.PI / 2, 0, 0]}>
+        <MyPieChart
+          data={data1}
+          position={[-8, -0.25, -5.2]}
+          radius={1.2}
+          title={"Dividend"}
+          depth={0.1}
+        />
+      </group>
+
+      <group scale={[0.3, 0.3, 0.3]}>
+        <Barchart
+          posx={14}
+          posy={12.5}
+          posz={-1.7}
+          length={7}
+          roty={0}
+          maxheight={7}
+        />
+      </group>
+
+      {/*Floor*/}
       <RoundedBox
         args={[22.5, 12, 0.25]}
         position={[-2, 0.1, 5]}
@@ -128,46 +244,6 @@ export default function Scene3({ textureCube }) {
           fontSize={0.6}
         ></Text>
       </RoundedBox>
-      <MyPieChart
-        data={data1}
-        position={[2, 0.5, 5]}
-        radius={1}
-        title={"Dividend"}
-        depth={0.1}
-      />
-      <MyPieChart
-        data={data1}
-        position={[2.5, 0.5, 9]}
-        radius={0.3}
-        title={"Dividend"}
-        depth={0.05}
-      />
-      <MyPieChart
-        data={data1}
-        position={[1.5, 0.5, 9]}
-        radius={0.3}
-        title={"Dividend"}
-        depth={0.05}
-      />
-
-      <group rotation={[Math.PI / 2, 0, 0]}>
-        <MyPieChart
-          data={data1}
-          position={[2, -0.25, -5.2]}
-          radius={1.2}
-          title={"Dividend"}
-          depth={0.1}
-        />
-      </group>
-      <group rotation={[Math.PI / 2, 0, 0]}>
-        <MyPieChart
-          data={data1}
-          position={[-8, -0.25, -5.2]}
-          radius={1.2}
-          title={"Dividend"}
-          depth={0.1}
-        />
-      </group>
     </>
   );
 }
